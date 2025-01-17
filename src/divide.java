0d9837ecce050beedb4350b5d90d8ae18442a01d
samridhi singh <gurjarsam6@gmail.com>
2025-01-17 14:20:54 +0530

    division of two numbers

---
+++ src/divide.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class divide {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        double num1 = sc.nextDouble();
+        System.out.println("Enter second number:");
+        double num2 = sc.nextDouble();
+        System.out.println(num1 / num2);
+    }}
