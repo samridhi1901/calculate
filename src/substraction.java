596bd0720b1e0facc915c56353ba83fed7210557
samridhi singh <gurjarsam6@gmail.com>
2025-01-17 14:08:54 +0530

    substraction of 2 numbers

---
+++ src/substraction.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class substraction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        double num1 = sc.nextDouble();
+        System.out.println("Enter second number:");
+        double num2 = sc.nextDouble();
+        System.out.print(num1 - num2);
+    }
+
+}
