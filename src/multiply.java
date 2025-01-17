964f344c6348d87446c0ccbc34d916639a254689
samridhi singh <gurjarsam6@gmail.com>
2025-01-17 14:15:16 +0530

    multiplication of 2 number

---
+++ src/multiply.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class multiply {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        double num1 = sc.nextDouble();
+        System.out.println("Enter second number:");
+        double num2 = sc.nextDouble();
+        System.out.println(num1 * num2);
+    }
+}
